10adc46c321ede210fb56e567e306f168a48d15f
Alexis Engelaere <alexis.engelaere.59@hotmail.fr>
2020-10-14 16:09:02 +0200

    Base de donnees

---
+++ restaurants.sql
@@ -0,0 +1,199 @@
+CREATE DATABASE  IF NOT EXISTS `yheilp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `yheilp`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: yheilp
+-- ------------------------------------------------------
+-- Server version	5.6.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `lieux`
+--
+
+DROP TABLE IF EXISTS `lieux`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lieux` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lieux`
+--
+
+LOCK TABLES `lieux` WRITE;
+/*!40000 ALTER TABLE `lieux` DISABLE KEYS */;
+INSERT INTO `lieux` VALUES (58,'Lille','France','Wow'),(59,'Lyon','France','Double Wow');
+/*!40000 ALTER TABLE `lieux` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restaurant`
+--
+
+DROP TABLE IF EXISTS `restaurant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `restaurant` (
+  `id` bigint(20) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+LOCK TABLES `restaurant` WRITE;
+/*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
+INSERT INTO `restaurant` VALUES (165,'http://jee.chticod.eu/img/lab06/angular_upandrunning.jpg','PANCOOK Lille','Cadre contemporain tamisé, mets traditionnels soignés et spécialité de pancook (pain fourré au poulet).');
+/*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `restaurant_lieux`
+--
+
+DROP TABLE IF EXISTS `restaurant_lieux`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `restaurant_lieux` (
+  `restaurant_id` bigint(20) NOT NULL,
+  `lieux_id` bigint(20) NOT NULL,
+  KEY `FKi58evljxutyg4crade5qq9chg` (`lieux_id`),
+  KEY `FKp9yng9x0cua3n2j4gfvlwpsc0` (`restaurant_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant_lieux`
+--
+
+LOCK TABLES `restaurant_lieux` WRITE;
+/*!40000 ALTER TABLE `restaurant_lieux` DISABLE KEYS */;
+INSERT INTO `restaurant_lieux` VALUES (165,58);
+/*!40000 ALTER TABLE `restaurant_lieux` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `note`
+--
+
+DROP TABLE IF EXISTS `note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `note` (
+  `id` bigint(20) NOT NULL,
+  /*`dateOfReview` datetime DEFAULT NULL,*/
+  `score` int(11) NOT NULL,
+  `restaurant_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK8d76td2o7d2d8foei85l93b9w` (`restaurant_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note`
+--
+
+LOCK TABLES `note` WRITE;
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+INSERT INTO `note` VALUES (245,4,165);
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `note_restaurant`
+--
+
+DROP TABLE IF EXISTS `note_restaurant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `note_restaurant` (
+  `note_id` bigint(20) NOT NULL,
+  `restaurant_id` bigint(20) NOT NULL,
+  KEY `FKi58evljxutyg4crade5qq9chg` (`restaurant_id`),
+  KEY `FKp9yng9x0cua3n2j4gfvlwpsc0` (`note_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note_restaurant`
+--
+
+LOCK TABLES `note_restaurant` WRITE;
+/*!40000 ALTER TABLE `note_restaurant` DISABLE KEYS */;
+INSERT INTO `note_restaurant` VALUES (245,165);
+/*!40000 ALTER TABLE `note_restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commentaire`
+--
+
+DROP TABLE IF EXISTS `commentaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commentaire` (
+  `id` bigint(20) NOT NULL,
+  /*`dateOfReview` datetime DEFAULT NULL,*/
+  `description` varchar(255) DEFAULT NULL,
+  `restaurant_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK8d76td2o7d2d8foei85l93b9w` (`restaurant_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commentaire`
+--
+
+LOCK TABLES `commentaire` WRITE;
+/*!40000 ALTER TABLE `commentaire` DISABLE KEYS */;
+INSERT INTO `commentaire` VALUES (34,'Quel bel endroit, je recommande !',165);
+/*!40000 ALTER TABLE `commentaire` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `commentaire_restaurant`
+--
+
+DROP TABLE IF EXISTS `commentaire_restaurant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commentaire_restaurant` (
+  `commentaire_id` bigint(20) NOT NULL,
+  `restaurant_id` bigint(20) NOT NULL,
+  KEY `FKi58evljxutyg4crade5qq9chg` (`restaurant_id`),
+  KEY `FKp9yng9x0cua3n2j4gfvlwpsc0` (`commentaire_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commentaire_restaurant`
+--
+
+LOCK TABLES `commentaire_restaurant` WRITE;
+/*!40000 ALTER TABLE `commentaire_restaurant` DISABLE KEYS */;
+INSERT INTO `commentaire_restaurant` VALUES (34,165);
+/*!40000 ALTER TABLE `commentaire_restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
